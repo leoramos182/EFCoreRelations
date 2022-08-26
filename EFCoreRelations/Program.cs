732bc47f9478e05d8bb7d0240939b1af0ea1662c
Leo Ramos Andrade <leo.ramos182@hotmail.com>
2022-08-26 13:55:35 -0300

    Adicionado swagger no projeto

--- EFCoreRelations/Program.cs
+++ EFCoreRelations/Program.cs
@@ -5,6 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen( s =>
+{
+    s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
+    {
+        Version = "v1",
+        Title = "Entity Framework Relations API",
+        Description = "API for tests with Entity Framework"
+    });
+
+});
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DataContext>(
     options => options.UseSqlServer(connectionString)
@@ -12,10 +23,18 @@ builder.Services.AddDbContext<DataContext>(
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Entity Framework Relations API");
+    c.RoutePrefix = String.Empty;
+});
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
